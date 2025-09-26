c15d4043a795856863110d06caa38eaef2a85be5
nGyu <vpdls1511@gmail.com>
2025-09-26 19:50:56 +0900

    feat : health체크용 enpoint 추가

---
+++ swiftpay-api/src/main/kotlin/com/ngyu/swiftpay/api/controller/HealthController.kt
@@ -0,0 +1,13 @@
+package com.ngyu.swiftpay.api.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(value = ["/health"], produces = ["application/json;charset=UTF-8"])
+class HealthController {
+
+  @GetMapping("/")
+  fun health(): String = "OK"
+}
