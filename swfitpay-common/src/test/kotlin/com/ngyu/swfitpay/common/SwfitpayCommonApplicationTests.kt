41fc59797b20dc0be1340665ffd327ba341dfe4e
nGyu <vpdls1511@gmail.com>
2025-09-28 13:48:48 +0900

    feat : common 모듈 생성

---
+++ swfitpay-common/src/test/kotlin/com/ngyu/swfitpay/common/SwfitpayCommonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ngyu.swfitpay.common
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SwfitpayCommonApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
