33f42c424ee78ab021bc04a4406c6702cb39e5d4
nGyu <vpdls1511@gmail.com>
2025-09-28 13:52:01 +0900

    refactor : 설정 수정

--- swiftpay-infrastructure/build.gradle.kts
+++ swiftpay-infrastructure/build.gradle.kts
@@ -29,6 +29,8 @@ dependencies {
   testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+  runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 }
 
 kotlin {
