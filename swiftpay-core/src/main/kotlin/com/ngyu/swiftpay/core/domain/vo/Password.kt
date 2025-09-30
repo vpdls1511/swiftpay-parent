c9bed0c10e5e08311a3032e23b2d8bd4f6ca7ba1
nGyu <vpdls1511@gmail.com>
2025-09-30 21:54:36 +0900

    feat : 비밀번호 검증을 위한 Password Vo 추가

---
+++ swiftpay-core/src/main/kotlin/com/ngyu/swiftpay/core/domain/vo/Password.kt
@@ -0,0 +1,26 @@
+package com.ngyu.swiftpay.core.domain.vo
+
+@JvmInline
+value class Password private constructor(val value: String) {
+  companion object {
+    fun of(password: String): Password {
+
+      require(password.isNotBlank()) { "비밀번호는 필수입니다." }
+      require(password.length >= 8) { "비밀번호는 최소 8자 이상이어야 합니다." }
+      require(password.any { it.isDigit() }) { "비밀번호는 최소 1개의 숫자를 포함해야 합니다." }
+      require(password.any { it.isUpperCase() }) { "비밀번호는 최소 1개의 대문자를 포함해야 합니다." }
+      require(password.any { it.isLowerCase() }) { "비밀번호는 최소 1개의 소문자를 포함해야 합니다." }
+      require(password.any { it in "@#$%^&+=!" }) { "비밀번호는 최소 1개의 특수문자를 포함해야 합니다." }
+
+      return Password(password)
+    }
+
+    fun ofEncrypt(encPassword: String): Password {
+      return Password(encPassword)
+    }
+  }
+
+  override fun toString(): String {
+    return "Password(*********)"
+  }
+}
