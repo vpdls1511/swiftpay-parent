c6407064dd6ae49c7b5814ae4d273e9c6944a28f
nGyu <vpdls1511@gmail.com>
2025-09-30 23:30:08 +0900

    fix : spring-security 최소 설정 추가

--- swiftpay-api/src/main/kotlin/com/ngyu/swiftpay/api/SwiftpayApiApplication.kt
+++ swiftpay-api/src/main/kotlin/com/ngyu/swiftpay/api/SwiftpayApiApplication.kt
@@ -1,13 +1,9 @@
 package com.ngyu.swiftpay.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(
-  scanBasePackages = ["com.ngyu.swiftpay"],
-  exclude = [SecurityAutoConfiguration::class]  // Security 자동 설정 제외
-)
+@SpringBootApplication(scanBasePackages = ["com.ngyu.swiftpay"])
 class SwiftpayApiApplication
 
 fun main(args: Array<String>) {
---
+++ swiftpay-security/src/main/kotlin/com/ngyu/swiftpay/security/SecurityConfig.kt
@@ -0,0 +1,25 @@
+package com.ngyu.swiftpay.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+  @Bean
+  fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+    http
+      .csrf { it.disable() }
+      .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+      .authorizeHttpRequests { auth ->
+        auth.anyRequest().permitAll()  // 모든 요청 허용
+      }
+
+    return http.build()
+  }
+}
