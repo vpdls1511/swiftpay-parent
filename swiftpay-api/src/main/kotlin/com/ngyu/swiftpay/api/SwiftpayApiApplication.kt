04e0d00fe685c6b9a33be1811a85e148b794e064
nGyu <vpdls1511@gmail.com>
2025-09-30 23:24:16 +0900

    feat : 임시로 spring-security 설정 무시

--- swiftpay-api/src/main/kotlin/com/ngyu/swiftpay/api/SwiftpayApiApplication.kt
+++ swiftpay-api/src/main/kotlin/com/ngyu/swiftpay/api/SwiftpayApiApplication.kt
@@ -1,9 +1,13 @@
 package com.ngyu.swiftpay.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(scanBasePackages = ["com.ngyu.swiftpay"])
+@SpringBootApplication(
+  scanBasePackages = ["com.ngyu.swiftpay"],
+  exclude = [SecurityAutoConfiguration::class]  // Security 자동 설정 제외
+)
 class SwiftpayApiApplication
 
 fun main(args: Array<String>) {
