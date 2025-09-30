e4f9a3132495375881e352c1cd4fc43b69b6b20e
nGyu <vpdls1511@gmail.com>
2025-09-30 23:03:10 +0900

    refactor : 설정 수정

--- swiftpay-api/build.gradle.kts
+++ swiftpay-api/build.gradle.kts
@@ -35,8 +35,8 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-validation")
 
   testImplementation("org.springframework.boot:spring-boot-starter-test")
-
   testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+  testImplementation("com.h2database:h2")
 
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
