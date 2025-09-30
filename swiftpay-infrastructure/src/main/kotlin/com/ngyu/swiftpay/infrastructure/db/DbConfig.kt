aa7f37cfbd17518c3059717764117248483dcc8a
nGyu <vpdls1511@gmail.com>
2025-09-30 23:24:33 +0900

    feat : 시간 자동생성을 위한 EnableJpaAuditing 설정

--- swiftpay-infrastructure/src/main/kotlin/com/ngyu/swiftpay/infrastructure/db/DbConfig.kt
+++ swiftpay-infrastructure/src/main/kotlin/com/ngyu/swiftpay/infrastructure/db/DbConfig.kt
@@ -1,4 +1,8 @@
 package com.ngyu.swiftpay.infrastructure.db
 
-class DbConfig {
-}
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class DbConfig
