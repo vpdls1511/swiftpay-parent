c1480c66e00174b429d8e28dd08ec51976a20a60
nGyu <vpdls1511@gmail.com>
2025-10-02 20:02:28 +0900

    fix : resposne 에 plain, hashed 값 같이 내려가던 문제 해결

--- module/security/src/main/kotlin/com/ngyu/swiftpay/security/provider/PaymentTokenProvider.kt
+++ module/security/src/main/kotlin/com/ngyu/swiftpay/security/provider/PaymentTokenProvider.kt
@@ -1,6 +1,5 @@
 package com.ngyu.swiftpay.security.provider
 
-import com.ngyu.swiftpay.core.constant.ServiceConstant
 import com.ngyu.swiftpay.security.util.HmacEncUtil
 import com.ngyu.swiftpay.security.vo.ApiKeyPair
 import org.springframework.beans.factory.annotation.Value
@@ -10,19 +9,6 @@ import org.springframework.stereotype.Component
 class PaymentTokenProvider(
   @Value("\${hmac.secret}") private val HMAC_KEY: String,
 ) {
-
-  fun issue(): ApiKeyPair {
-    val signature = HmacEncUtil.generate(HMAC_KEY)
-    val apiKey = "${ServiceConstant.SERVICE_NAME}.${signature}"
-
-    return ApiKeyPair(
-      plain = apiKey,
-      hashed = HmacEncUtil.hash(apiKey)
-    )
-  }
-
-  fun verify(apiKey: String, hashKey: String) : Boolean {
-    return HmacEncUtil.verify(apiKey, hashKey)
-  }
-
+  fun issue(): ApiKeyPair = HmacEncUtil.generate(HMAC_KEY)
+  fun verify(apiKey: String, hashKey: String) : Boolean = HmacEncUtil.verify(apiKey, hashKey)
 }
