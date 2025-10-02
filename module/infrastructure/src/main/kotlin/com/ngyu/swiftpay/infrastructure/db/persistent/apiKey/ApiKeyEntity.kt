e2b3770d1d0306fb7e2c237dab941fbc759a736a
nGyu <vpdls1511@gmail.com>
2025-10-02 17:08:44 +0900

    feat : ApiKey Entity 추가

---
+++ module/infrastructure/src/main/kotlin/com/ngyu/swiftpay/infrastructure/db/persistent/apiKey/ApiKeyEntity.kt
@@ -0,0 +1,38 @@
+package com.ngyu.swiftpay.infrastructure.db.persistent.apiKey
+
+import com.ngyu.swiftpay.core.domain.apiKey.ApiKeyStatus
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "api_key")
+class ApiKeyEntity(
+  @Id
+  @Column(name = "api_key", unique = true, nullable = false)
+  val apiKey: String,
+
+  @Column(name = "user_id")
+  val userId: Long? = null,
+
+  @Column(name = "call_limit", nullable = false)
+  val callLimit: Int,
+
+  @Column(name = "issued_at", nullable = false)
+  val issuedAt: LocalDateTime,
+
+  @Column(name = "expires_at", nullable = false)
+  val expiresAt: LocalDateTime,
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false, length = 20)
+  val status: ApiKeyStatus
+) {
+  protected constructor() : this(
+    apiKey = "",
+    userId = null,
+    callLimit = 0,
+    issuedAt = LocalDateTime.now(),
+    expiresAt = LocalDateTime.now(),
+    status = ApiKeyStatus.ACTIVE
+  )
+}
