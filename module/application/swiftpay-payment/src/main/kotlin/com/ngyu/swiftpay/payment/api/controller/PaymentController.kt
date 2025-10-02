560649dc03004461b6aa8e72c644a05207d7a76a
nGyu <vpdls1511@gmail.com>
2025-10-02 13:46:40 +0900

    feat : api key 를 발급받기 위한 컨트롤러 생성

---
+++ module/application/swiftpay-payment/src/main/kotlin/com/ngyu/swiftpay/payment/api/controller/PaymentController.kt
@@ -0,0 +1,22 @@
+package com.ngyu.swiftpay.payment.api.controller
+
+import com.ngyu.swiftpay.payment.api.application.PaymentApiKeyUseCase
+import com.ngyu.swiftpay.payment.api.dto.ApiKeyResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/payment")
+class PaymentController(
+  private val paymentApiKeyUseCase: PaymentApiKeyUseCase,
+) {
+
+  @PostMapping(value = ["/key/issued"])
+  fun issuedKey(): ResponseEntity<ApiKeyResponse> {
+    val response = paymentApiKeyUseCase.issueKey()
+    return ResponseEntity.ok(response)
+  }
+
+}
