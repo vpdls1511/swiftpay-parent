edc46da7351aa3b95f4b32e2943f392b1810548e
nGyu <vpdls1511@gmail.com>
2025-10-02 17:10:01 +0900

    feat : 토큰발급 후 DB에 저장할 수 있음

--- module/application/payment/src/main/kotlin/com/ngyu/swiftpay/payment/api/application/PaymentApiKeyService.kt
+++ module/application/payment/src/main/kotlin/com/ngyu/swiftpay/payment/api/application/PaymentApiKeyService.kt
@@ -1,15 +1,24 @@
 package com.ngyu.swiftpay.payment.api.application
 
+import com.ngyu.swiftpay.core.domain.apiKey.ApiKey
+import com.ngyu.swiftpay.infrastructure.db.persistent.apiKey.ApiKeyEntity
+import com.ngyu.swiftpay.infrastructure.db.persistent.apiKey.ApiKeyMapper
+import com.ngyu.swiftpay.infrastructure.db.persistent.apiKey.ApiKeyRepositoryAdapter
 import com.ngyu.swiftpay.payment.api.dto.ApiKeyResponse
 import com.ngyu.swiftpay.security.provider.PaymentTokenProvider
 import org.springframework.stereotype.Service
 
 @Service
 class PaymentApiKeyService(
-  private val paymentTokenProvider: PaymentTokenProvider
+  private val paymentTokenProvider: PaymentTokenProvider,
+  private val apiKeyRepositoryAdapter: ApiKeyRepositoryAdapter
 ) : PaymentApiKeyUseCase {
   override fun issueKey(): ApiKeyResponse {
-    val key = paymentTokenProvider.issue()
+    val key: String = paymentTokenProvider.issue()
+    val apiKey: ApiKey = ApiKey.create(key)
+
+    val apiKeyEntity: ApiKeyEntity = ApiKeyMapper.toEntity(apiKey)
+    apiKeyRepositoryAdapter.save(apiKeyEntity)
 
     return ApiKeyResponse(key)
   }
