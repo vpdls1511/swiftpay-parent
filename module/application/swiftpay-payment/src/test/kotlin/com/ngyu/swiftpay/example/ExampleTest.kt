d5b2b266ff6dddf0e648268d41a0cc11fcab03f5
nGyu <vpdls1511@gmail.com>
2025-10-02 14:22:25 +0900

    test : 예제 테스트코드 추가

---
+++ module/application/swiftpay-payment/src/test/kotlin/com/ngyu/swiftpay/example/ExampleTest.kt
@@ -0,0 +1,24 @@
+package com.ngyu.swiftpay.example
+
+import com.ngyu.swiftpay.core.domain.extension.logger
+import org.junit.jupiter.api.Test
+
+class ExampleTest {
+  private val log = logger()
+
+  @Test
+  fun test() {
+    //given
+    val logText = "로그 출력 테스트"
+
+    //when
+    log.trace(logText)
+    log.debug(logText)
+    log.info(logText)
+    log.warn(logText)
+    log.error(logText)
+
+    //then
+    //콘솔에서 출력되는 로그 확인
+  }
+}
