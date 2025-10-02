d7deace0882795002c8ba1eaaa3035464eca0762
nGyu <vpdls1511@gmail.com>
2025-10-02 19:39:49 +0900

    test : Bcrypt로 해싱 후 verify 체크를 위한 테스트

--- module/application/payment/src/test/kotlin/com/ngyu/swiftpay/payment/service/PaymentApiKeyServiceTest.kt
+++ module/application/payment/src/test/kotlin/com/ngyu/swiftpay/payment/service/PaymentApiKeyServiceTest.kt
@@ -3,8 +3,8 @@ package com.ngyu.swiftpay.payment.service
 import com.ngyu.swiftpay.security.provider.PaymentTokenProvider
 import com.ngyu.swiftpay.security.util.HmacEncUtil
 import org.assertj.core.api.AssertionsForClassTypes.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 
 class PaymentApiKeyServiceTest {
@@ -12,32 +12,37 @@ class PaymentApiKeyServiceTest {
   private val SECRET_KEY: String = "ck7488ZlcflJ+ZwsY/h9vIzPRjS5KsuAlQS9lBVBgks="
   private val paymentTokenProvider: PaymentTokenProvider = PaymentTokenProvider(SECRET_KEY)
 
-  private var apiTestToken: String = ""
+  private lateinit var apiKeyPair: HmacEncUtil.ApiKeyPair
 
-  @Test
-  @Order(1)
-  @DisplayName("Api 키 생성이 가능함")
-  fun createApiKey() {
-    //given
+  @BeforeEach
+  fun setup() {
     val signature: HmacEncUtil.ApiKeyPair = paymentTokenProvider.issue()
-
-    //when
-    apiTestToken = signature.plain
-
-    //then
-    assertThat(apiTestToken).isNotEmpty()
+    apiKeyPair = signature
   }
 
   @Test
   @DisplayName("Api 키 검증 성공")
   fun successVerifyApiKey() {
     // given
-    val plain = apiTestToken
+    val pair = apiKeyPair
 
     // when
-    val verify: Boolean = paymentTokenProvider.verify(plain)
+    val verify: Boolean = paymentTokenProvider.verify(pair.plain, pair.hashed)
 
     // then
     assertThat(verify).isTrue()
+  }
+
+  @Test
+  @DisplayName("Api 키 검증 싪패")
+  fun failureVerifyApiKey() {
+    // given
+    val pair = apiKeyPair
+
+    // when
+    val verify: Boolean = paymentTokenProvider.verify("asdfasdfasdfasdfasdfasdf", pair.hashed)
+
+    // then
+    assertThat(verify).isFalse()
   }
 }
