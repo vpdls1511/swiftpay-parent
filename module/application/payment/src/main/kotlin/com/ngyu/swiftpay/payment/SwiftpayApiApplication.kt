2327cfa826260035d8cd2ff6d02048a39b78ab6a
nGyu <vpdls1511@gmail.com>
2025-10-02 17:23:52 +0900

    feat : infra 모듈에서 db r/w를 위해  EntityScan, EnableJpaRepositories 추가

--- module/application/payment/src/main/kotlin/com/ngyu/swiftpay/payment/SwiftpayApiApplication.kt
+++ module/application/payment/src/main/kotlin/com/ngyu/swiftpay/payment/SwiftpayApiApplication.kt
@@ -1,8 +1,12 @@
 package com.ngyu.swiftpay.payment
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
+@EntityScan(basePackages = ["com.ngyu.swiftpay.infrastructure.db.persistent"])
+@EnableJpaRepositories(basePackages = ["com.ngyu.swiftpay.infrastructure.db.persistent"])
 @SpringBootApplication(scanBasePackages = ["com.ngyu.swiftpay"])
 class SwiftpayApiApplication
 
