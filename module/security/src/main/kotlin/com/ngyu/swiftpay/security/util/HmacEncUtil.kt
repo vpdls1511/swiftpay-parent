e47e5f95a717d27b4c8e83abf584566005f78de3
nGyu <vpdls1511@gmail.com>
2025-10-02 19:41:06 +0900

    feat : BCrypt 사용을 위해 SHA-256으로 hash 값 64바이트로 변환

--- module/security/src/main/kotlin/com/ngyu/swiftpay/security/util/HmacEncUtil.kt
+++ module/security/src/main/kotlin/com/ngyu/swiftpay/security/util/HmacEncUtil.kt
@@ -1,6 +1,8 @@
 package com.ngyu.swiftpay.security.util
 
+import com.ngyu.swiftpay.core.extension.logger
 import org.springframework.security.crypto.bcrypt.BCrypt
+import java.security.MessageDigest
 import java.security.SecureRandom
 import java.util.*
 import javax.crypto.Mac
@@ -8,8 +10,11 @@ import javax.crypto.spec.SecretKeySpec
 
 object HmacEncUtil {
 
-  data class ApiKeyPair (val plain: String, val hashed: String )
+  private val log = logger()
 
+  /**
+   * 시크릿 키를 기반으로 HMAC과 hash값 전달
+   */
   fun generate(secretKey: String): ApiKeyPair {
     val timestamp = System.currentTimeMillis()
     val random = SecureRandom().nextInt().toString()
@@ -21,9 +26,31 @@ object HmacEncUtil {
     return ApiKeyPair(signature, hashKey)
   }
 
-  fun hash(apiKey: String): String = BCrypt.hashpw(apiKey, BCrypt.gensalt())
-  fun verify(apiKey: String, hashKey: String): Boolean = BCrypt.checkpw(apiKey, hashKey)
+  /**
+   * api key 값을 DB에 저장하기 위해 hash
+   */
+  fun hash(apiKey: String): String {
+    val sha256hash = sha256(apiKey)
 
+    log.trace("plain key: $apiKey")
+    log.trace("hash key: $sha256hash")
+
+    return BCrypt.hashpw(sha256hash, BCrypt.gensalt())
+  }
+
+
+  /**
+   * DB에 저장된 hash 값과 api request key 검증
+   */
+  fun verify(apiKey: String, hashKey: String): Boolean {
+    val sha256hash = sha256(apiKey)
+
+    return BCrypt.checkpw(sha256hash, hashKey)
+  }
+
+  /**
+   * data 를 hmac 으로 변환
+   */
   private fun hmac(data: String, secretKey: String): String {
     val hmac = Mac.getInstance("HmacSHA256")
     hmac.init(SecretKeySpec(secretKey.toByteArray(), "HmacSHA256"))
@@ -32,4 +59,18 @@ object HmacEncUtil {
       .withoutPadding()
       .encodeToString(hmac.doFinal(data.toByteArray()))
   }
+
+  /**
+   * SHA-256으로 변환을 위한 메서드
+   *
+   *
+   * BCrypt 는 72바이트까지만 가능하기 때문에.. HMAC 값을 SHA-256으로 한번 더 해싱
+   */
+  private fun sha256(data: String): String {
+    return MessageDigest.getInstance("SHA-256")
+      .digest(data.toByteArray())
+      .joinToString("") { String.format("%02x", it) }
+  }
+
+  data class ApiKeyPair (val plain: String, val hashed: String )
 }
