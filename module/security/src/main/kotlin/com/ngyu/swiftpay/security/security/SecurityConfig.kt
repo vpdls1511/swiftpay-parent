fd9323c952baa86855a6599aa97b19bc2f79aab7
nGyu <vpdls1511@gmail.com>
2025-10-06 15:17:43 +0900

    refactor : 시큐리티 설정파일 위치 변경

--- module/security/src/main/kotlin/com/ngyu/swiftpay/security/security/SecurityConfig.kt
+++ module/security/src/main/kotlin/com/ngyu/swiftpay/security/security/SecurityConfig.kt
@@ -1,4 +1,4 @@
-package com.ngyu.swiftpay.security
+package com.ngyu.swiftpay.security.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
