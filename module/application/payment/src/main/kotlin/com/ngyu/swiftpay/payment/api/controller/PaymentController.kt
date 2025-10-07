f9ccf7612afc1fe468fa88d6f0d337428312dc95
nGyu <vpdls1511@gmail.com>
2025-10-07 17:48:05 +0900

    feat : 토큰의 유효성 체크를 할 수 있는 컨트롤러 추가

--- module/application/payment/src/main/kotlin/com/ngyu/swiftpay/payment/api/controller/PaymentController.kt
+++ module/application/payment/src/main/kotlin/com/ngyu/swiftpay/payment/api/controller/PaymentController.kt
@@ -1,5 +1,9 @@
 package com.ngyu.swiftpay.payment.api.controller
 
+import com.ngyu.swiftpay.core.logger.logger
+import com.ngyu.swiftpay.payment.security.PaymentPrincipal
+import com.ngyu.swiftpay.payment.security.vo.PaymentCredentialsVo
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -7,4 +11,13 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/payment")
 class PaymentController(
 ) {
+
+  private val log = logger()
+
+  @PostMapping("/health")
+  fun processPayment(
+    @PaymentPrincipal principal: PaymentCredentialsVo
+  ): String {
+    return "ok"
+  }
 }
