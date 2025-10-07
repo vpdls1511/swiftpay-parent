df2600a51febf801b95dd354e13dfb3ddabf0655
nGyu <vpdls1511@gmail.com>
2025-10-07 17:08:22 +0900

    feat : payment 모듈의 security 설정

---
+++ module/application/payment/src/main/kotlin/com/ngyu/swiftpay/payment/security/PaymentSecurityConfig.kt
@@ -0,0 +1,29 @@
+package com.ngyu.swiftpay.payment.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+@Configuration
+@EnableWebSecurity
+class PaymentSecurityConfig(
+  private val paymentTokenValidator: PaymentTokenValidator
+) {
+
+  @Bean
+  fun paymentSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+    return http
+      .securityMatcher("/api/payment/**")
+      .addFilterBefore(
+        PaymentTokenFilter(paymentTokenValidator),
+        UsernamePasswordAuthenticationFilter::class.java
+      )
+      .csrf { it.disable() }
+      .build()
+
+  }
+
+}
