1a41000c1ac49de178a74d4aa5cc8a289beaebc5
nGyu <vpdls1511@gmail.com>
2025-10-07 17:08:02 +0900

    chore : security 의존성 전파를 위해 api 로 선언

--- module/security/build.gradle.kts
+++ module/security/build.gradle.kts
@@ -34,7 +34,9 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter")
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
-  implementation("org.springframework.boot:spring-boot-starter-security")
+
+  api("org.springframework.boot:spring-boot-starter-security")
+
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
