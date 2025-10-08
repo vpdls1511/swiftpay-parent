6668fd55c83c5959699ec8ef2f980f1c0780493f
nGyu <vpdls1511@gmail.com>
2025-10-08 11:46:36 +0900

    feat : swagger 에서 PaymentPrincipal 안받아도 되게끔 수정

--- module/application/payment/src/main/kotlin/com/ngyu/swiftpay/payment/security/PaymentPrincipal.kt
+++ module/application/payment/src/main/kotlin/com/ngyu/swiftpay/payment/security/PaymentPrincipal.kt
@@ -1,5 +1,8 @@
 package com.ngyu.swiftpay.payment.security
 
+import io.swagger.v3.oas.annotations.Parameter
+
 @Target(AnnotationTarget.VALUE_PARAMETER)
 @Retention(AnnotationRetention.RUNTIME)
+@Parameter(hidden = true)
 annotation class PaymentPrincipal
