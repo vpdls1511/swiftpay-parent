211e6318cb1b1288271fb52950c47b7175513b58
nGyu <vpdls1511@gmail.com>
2025-10-08 22:07:27 +0900

    refactor : BaseAuthenticationFilter 에서는 예외만 던질 수 있도록 수정

--- module/security/src/main/kotlin/com/ngyu/swiftpay/security/security/BaseAuthenticationFilter.kt
+++ module/security/src/main/kotlin/com/ngyu/swiftpay/security/security/BaseAuthenticationFilter.kt
@@ -1,12 +1,10 @@
 package com.ngyu.swiftpay.security.security
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.ngyu.swiftpay.core.exception.response.ExceptionResponse
+import com.ngyu.swiftpay.core.exception.PrincipalException
 import com.ngyu.swiftpay.core.logger.logger
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import org.springframework.http.MediaType
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.util.AntPathMatcher
@@ -72,20 +70,10 @@ abstract class BaseAuthenticationFilter<T> : OncePerRequestFilter() {
     response: HttpServletResponse,
     filterChain: FilterChain
   ) {
-    val credentials = extractCredentials(request)
-
-    if (credentials == null) {
-      log.error("인증 정보가 없습니다.")
-//      response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "인증이 필요합니다.")
-      exceptionResponse(response, request, HttpServletResponse.SC_UNAUTHORIZED, "인증 정보가 없습니다.")
-      return
-    }
+    val credentials = extractCredentials(request) ?: throw PrincipalException("인증 정보가 없습니다.")
 
     if (!validateCredentials(credentials)) {
-      log.error("유효하지 않은 인증 정보 입니다.")
-//      response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "인증에 실패했습니다.")
-      exceptionResponse(response, request, HttpServletResponse.SC_UNAUTHORIZED, "유효하지 않은 인증 정보 입니다.")
-      return
+      throw PrincipalException("유효하지 않은 인증 정보 입니다.")
     }
 
     val authentication = UsernamePasswordAuthenticationToken(
@@ -97,26 +85,5 @@ abstract class BaseAuthenticationFilter<T> : OncePerRequestFilter() {
     SecurityContextHolder.getContext().authentication = authentication
 
     filterChain.doFilter(request, response)
-  }
-
-  private fun exceptionResponse(
-    response: HttpServletResponse,
-    request: HttpServletRequest,
-    code: Int,
-    message: String
-  ) {
-    response.status = code
-    response.contentType = MediaType.APPLICATION_JSON_VALUE
-    response.characterEncoding = "UTF-8"
-
-    try {
-      val json = ObjectMapper().writeValueAsString(
-        ExceptionResponse.create(message = message, request = request)
-      )
-
-      response.writer.write(json)
-    } catch (ex: Exception) {
-      log.error(ex.message, ex)
-    }
   }
 }
