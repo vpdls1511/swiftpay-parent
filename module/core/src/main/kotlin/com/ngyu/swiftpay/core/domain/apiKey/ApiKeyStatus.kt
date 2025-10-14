9435a348af1cbc0536cd7446b42cddb2757151b7
nGyu <vpdls1511@gmail.com>
2025-10-14 22:45:28 +0900

    refactor : value 관리 필요없어서 삭제

--- module/core/src/main/kotlin/com/ngyu/swiftpay/core/domain/apiKey/ApiKeyStatus.kt
+++ module/core/src/main/kotlin/com/ngyu/swiftpay/core/domain/apiKey/ApiKeyStatus.kt
@@ -1,6 +1,6 @@
 package com.ngyu.swiftpay.core.domain.apiKey
 
-enum class ApiKeyStatus( value: String ) {
-  ACTIVE("ACTIVE"),
-  INACTIVE("INACTIVE"),
+enum class ApiKeyStatus {
+  ACTIVE,
+  INACTIVE,
 }
