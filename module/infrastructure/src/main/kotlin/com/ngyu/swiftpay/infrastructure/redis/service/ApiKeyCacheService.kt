de7c937dfda998d8295fd1eb072910889825bd35
nGyu <vpdls1511@gmail.com>
2025-10-14 22:45:53 +0900

    feat : ApiKey 도메인을 저장하기 위해 구조 수정

--- module/infrastructure/src/main/kotlin/com/ngyu/swiftpay/infrastructure/redis/service/ApiKeyCacheService.kt
+++ module/infrastructure/src/main/kotlin/com/ngyu/swiftpay/infrastructure/redis/service/ApiKeyCacheService.kt
@@ -1,5 +1,8 @@
 package com.ngyu.swiftpay.infrastructure.redis.service
 
+import com.ngyu.swiftpay.core.domain.apiKey.ApiKey
+import com.ngyu.swiftpay.core.domain.apiKey.ApiKeyStatus
+import com.ngyu.swiftpay.core.exception.PrincipalException
 import com.ngyu.swiftpay.infrastructure.redis.constant.RedisKey
 import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.stereotype.Service
@@ -12,29 +15,29 @@ class ApiKeyCacheService(
 ) {
   private val hashOps = redisTemplate.opsForHash<String, String>()
 
-  fun save(apiKey: String,
-           apiPairKey: String,
-           expiredAt: LocalDateTime,
-           callLimit: Int
-           ) {
-    val key = RedisKey.apiKey(apiPairKey)
+  fun save(apiKey: ApiKey) {
+    val key = RedisKey.apiKey(apiKey.lookupKey)
 
     val map = mapOf(
-      "apiKey" to apiKey,
-      "expiredAt" to expiredAt.toString(),
-      "callLimit" to callLimit.toString(),
+      "apiKey" to apiKey.apiKey,
+      "lookupKey" to apiKey.lookupKey,
+      "userId" to (apiKey.userId.toString() ?: ""),
+      "issuedAt" to apiKey.issuedAt.toString(),
+      "expiresAt" to apiKey.expiresAt.toString(),
+      "status" to apiKey.status.toString(),
     )
-    val duration = Duration.between(LocalDateTime.now(), expiredAt).coerceAtLeast(Duration.ZERO)
 
     hashOps.putAll(key, map)
+
+    val duration = Duration.between(LocalDateTime.now(), apiKey.expiresAt).coerceAtLeast(Duration.ZERO)
     redisTemplate.expire(key, duration)
   }
 
-  fun find(apiPairKey: String): Map<String, String>? {
+  fun find(apiPairKey: String): ApiKey? {
     val key = RedisKey.apiKey(apiPairKey)
     val entries = hashOps.entries(key)
 
-    return if (entries.isEmpty()) null else entries
+    return if (entries.isEmpty()) null else entries.toDomain()
   }
 
   // TODO - 가능성은 미미하나, 만약 RaceCondition 발생 시 HINCRBY 적용하기
@@ -51,5 +54,19 @@ class ApiKeyCacheService(
   fun delete(apiPairKey: String) {
     val key = RedisKey.apiKey(apiPairKey)
     redisTemplate.delete(key)
+  }
+
+  private fun Map<String, String>.toDomain(): ApiKey {
+    return ApiKey(
+      apiKey = this["apiKey"] ?: throw PrincipalException("apiKey가 없습니다."),
+      lookupKey = this["lookupKey"] ?: throw PrincipalException("lookupKey가 없습니다."),
+      userId = this["userId"]?.toLong(),
+      callLimit = this["callLimit"]?.toIntOrNull() ?: 0,
+      issuedAt = this["issuedAt"]?.let { LocalDateTime.parse(it) }
+        ?: throw PrincipalException("issuedAt이 없습니다."),
+      expiresAt = this["expiresAt"]?.let { LocalDateTime.parse(it) }
+        ?: throw PrincipalException("expiresAt이 없습니다."),
+      status = (this["status"] ?: ApiKeyStatus.INACTIVE) as ApiKeyStatus,
+    )
   }
 }
