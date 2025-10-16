e26ecfd269b635fde516a8aa0cb29d4be4549917
nGyu <vpdls1511@gmail.com>
2025-10-17 00:11:07 +0900

    feat : 가맹점 생성 요청을 받을 수 있는 컨트롤러 생성

---
+++ module/application/payment/src/main/kotlin/com/ngyu/swiftpay/payment/api/controller/MerchantController.kt
@@ -0,0 +1,31 @@
+package com.ngyu.swiftpay.payment.api.controller
+
+import com.ngyu.swiftpay.payment.api.application.service.MerchantService
+import com.ngyu.swiftpay.payment.api.dto.MerchantRegisterReqeust
+import com.ngyu.swiftpay.payment.api.dto.PaymentCredentials
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@Tag(name = "Merchant API")
+@RestController
+@RequestMapping("/merchant")
+class MerchantController(
+  private val merchantService: MerchantService
+) {
+
+  @PostMapping("/register")
+  @Operation(summary = "가맹점 등록", description = "가맹점을 등록할 수 있습니다. 원래는 승인이 필요하나, 개발단계에서는 등록과 동시에 자동승인")
+  fun register(
+    @RequestBody request: MerchantRegisterReqeust
+  ): ResponseEntity<PaymentCredentials> {
+    val response = merchantService.register(request)
+    return ResponseEntity.ok(response)
+  }
+
+}
