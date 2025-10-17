732e5613124cf82a87c4281f0b4052a9c41a31aa
nGyu <vpdls1511@gmail.com>
2025-10-17 23:14:07 +0900

    feat : 가맹점 예외처리 핸들러 추가

---
+++ module/core/src/main/kotlin/com/ngyu/swiftpay/core/exception/handler/MerchantExceptionHandler.kt
@@ -0,0 +1,38 @@
+package com.ngyu.swiftpay.core.exception.handler
+
+import com.ngyu.swiftpay.core.exception.MerchantException
+import com.ngyu.swiftpay.core.exception.response.ExceptionResponse
+import com.ngyu.swiftpay.core.logger.logger
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+/**
+ * 가맹점 관련한 예외를 처리하는 글로벌 핸들러
+ *
+ * core 모듈을 의존하는 모든 프로젝트의 @RestController에서 발생하는
+ * 가맹점 관련 Exception을 캐치하여 500 InternalServerError 응답을 반환한다.
+ */
+@RestControllerAdvice
+class MerchantExceptionHandler {
+
+  private val log = logger()
+
+  @ExceptionHandler(MerchantException::class)
+  fun handleMerchantException(
+    e: MerchantException,
+    request: HttpServletRequest
+  ): ResponseEntity<ExceptionResponse> {
+    log.error("MerchantException = ${e.message}")
+    return ResponseEntity
+      .status(HttpStatus.INTERNAL_SERVER_ERROR)
+      .body(
+        ExceptionResponse.create(
+          message = e.message ?: "MerchantException",
+          request = request
+        )
+      )
+  }
+}
