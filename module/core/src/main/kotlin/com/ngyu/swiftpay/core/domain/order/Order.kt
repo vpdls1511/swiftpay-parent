bfa6cd9892dbb26d7972a1bb958d86980e696340
nGyu <vpdls1511@gmail.com>
2025-10-17 21:44:32 +0900

    feat : 세금, 공급가액 추가 후 부분환불을 위한 부분환불 로직 추가

--- module/core/src/main/kotlin/com/ngyu/swiftpay/core/domain/order/Order.kt
+++ module/core/src/main/kotlin/com/ngyu/swiftpay/core/domain/order/Order.kt
@@ -9,7 +9,11 @@ data class Order(
   val merchantId: String,
 
   val orderName: String,
+
   val totalAmount: Money,
+  val balanceAmount: Money,
+  val supplyAmount: Money,
+  val tax: Money,
 
   val status: OrderStatus,
 
@@ -21,7 +25,10 @@ data class Order(
   val createdAt: LocalDateTime,
   val updatedAt: LocalDateTime,
 ) {
+
   companion object {
+    val taxFee = 11L
+
     fun create(
       merchantId: String,
       orderName: String,
@@ -30,11 +37,17 @@ data class Order(
       customerEmail: String? = null,
       customerPhone: String? = null
     ): Order {
+      val tax = totalAmount / taxFee
+      val supplyAmount = totalAmount - tax
+
       return Order(
         orderId = UUID.randomUUID().toString(),
         merchantId = merchantId,
         orderName = orderName,
         totalAmount = totalAmount,
+        balanceAmount = totalAmount,
+        supplyAmount = supplyAmount,
+        tax = tax,
         status = OrderStatus.READY,
         customerName = customerName,
         customerEmail = customerEmail,
@@ -43,11 +56,37 @@ data class Order(
         updatedAt = LocalDateTime.now()
       )
     }
-
   }
+
+  fun refund(amount: Money): Order {
+    require(status == OrderStatus.DONE || status == OrderStatus.PARTIAL_REFUNDED) { "환불 가능한 상태가 아닙니다." }
+    require(balanceAmount >= amount) { "환불 금액이 환불 가능 금액보다 큽니다." }
+
+    val refundTax = amount / taxFee
+    val refundSupply = amount - refundTax
+    val newBalance = this.balanceAmount - amount
+
+    val status = if (newBalance == Money.ZERO) {
+      OrderStatus.REFUNDED
+    } else {
+      OrderStatus.PARTIAL_REFUNDED
+    }
+
+    return this.copy(
+      status = status,
+      balanceAmount = newBalance,
+      tax = this.tax - refundTax,
+      supplyAmount = this.supplyAmount - refundSupply,
+      updatedAt = LocalDateTime.now(),
+    )
+  }
+
   fun processing(): Order {
     require(this.status == OrderStatus.READY) { "주문 대기 상태가 아닙니다." }
-    return this.copy(status = OrderStatus.PROCESSING)
+    return this.copy(
+      status = OrderStatus.PROCESSING,
+      updatedAt = LocalDateTime.now()
+    )
   }
 
   fun done(): Order {
@@ -68,8 +107,10 @@ data class Order(
 }
 
 enum class OrderStatus {
-  READY,        // 주문 생성됨
-  PROCESSING,   // 주문 진행중
-  DONE,         // 결제 완료
-  CANCELLED     // 취소됨
+  READY,              // 주문 생성됨
+  PROCESSING,         // 주문 진행중
+  DONE,               // 결제 완료
+  PARTIAL_REFUNDED,   // 부분 환불
+  REFUNDED,           // 전액 환불
+  CANCELLED,          // 취소됨
 }
