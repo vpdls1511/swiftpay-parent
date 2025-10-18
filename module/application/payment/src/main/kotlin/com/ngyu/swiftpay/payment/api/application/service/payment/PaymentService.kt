45bd5abb71d7092425d9017dc3f43f696f775525
nGyu <vpdls1511@gmail.com>
2025-10-18 11:13:09 +0900

    feat : 전략패턴을 통한 결제방법별 분기처리

--- module/application/payment/src/main/kotlin/com/ngyu/swiftpay/payment/api/application/service/payment/PaymentService.kt
+++ module/application/payment/src/main/kotlin/com/ngyu/swiftpay/payment/api/application/service/payment/PaymentService.kt
@@ -1,15 +1,49 @@
 package com.ngyu.swiftpay.payment.api.application.service.payment
 
+import com.ngyu.swiftpay.core.domain.payment.Payment
+import com.ngyu.swiftpay.payment.api.application.strategy.PaymentStrategyFactory
 import com.ngyu.swiftpay.payment.api.application.usecase.PaymentUseCase
 import com.ngyu.swiftpay.payment.api.dto.OrderCreateRequestDto
 import com.ngyu.swiftpay.payment.api.dto.OrderCreateResponseDto
+import com.ngyu.swiftpay.payment.api.dto.PaymentRequestDto
+import com.ngyu.swiftpay.payment.api.dto.PaymentResponseDto
 import org.springframework.stereotype.Service
 
 @Service
-class PaymentService: PaymentUseCase {
+class PaymentService(
+  private val paymentStrategyFactory: PaymentStrategyFactory
+) : PaymentUseCase {
   override fun readyOrder(request: OrderCreateRequestDto): OrderCreateResponseDto {
     val domain = request.toDomain()
 
     return OrderCreateResponseDto.fromDomain(domain)
+  }
+
+  override fun processing(request: PaymentRequestDto): PaymentResponseDto {
+    val domain = this.savePayment(request)
+    val strategy = paymentStrategyFactory.getStrategy(domain)
+
+    val result = if (strategy.shouldAsyncProcessing(domain)) {
+      this.processAsync()
+    } else {
+      this.processSync()
+    }
+
+    return PaymentResponseDto.fromDomain(domain)
+  }
+
+  private fun savePayment(request: PaymentRequestDto): Payment {
+    val domain = request.toDomain()
+    var updateDomain = domain.inProgress()
+    // TODO("추후 Repository 연결 시 주석 삭제해야함")
+    return updateDomain
+  }
+
+  private fun processAsync() {
+
+  }
+
+  private fun processSync() {
+
   }
 }
