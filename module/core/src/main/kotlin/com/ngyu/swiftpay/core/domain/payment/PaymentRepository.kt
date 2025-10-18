6b6670c2ff12cd748ab47af8a3b08323bfc91251
nGyu <vpdls1511@gmail.com>
2025-10-18 11:21:54 +0900

    feat : DB r/w를 위한 Repository 메서드 추가

--- module/core/src/main/kotlin/com/ngyu/swiftpay/core/domain/payment/PaymentRepository.kt
+++ module/core/src/main/kotlin/com/ngyu/swiftpay/core/domain/payment/PaymentRepository.kt
@@ -1,4 +1,6 @@
 package com.ngyu.swiftpay.core.domain.payment
 
 interface PaymentRepository {
+  fun save(domain: Payment): Payment
+  fun findByPaymentId(domain: Payment): Payment
 }
--- module/infrastructure/src/main/kotlin/com/ngyu/swiftpay/infrastructure/db/persistent/payment/PaymentRepositoryAdapter.kt
+++ module/infrastructure/src/main/kotlin/com/ngyu/swiftpay/infrastructure/db/persistent/payment/PaymentRepositoryAdapter.kt
@@ -1,10 +1,26 @@
 package com.ngyu.swiftpay.infrastructure.db.persistent.payment
 
+import com.ngyu.swiftpay.core.domain.payment.Payment
 import com.ngyu.swiftpay.core.domain.payment.PaymentRepository
+import com.ngyu.swiftpay.infrastructure.db.persistent.payment.mapper.PaymentMapper
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
 
 @Component
 class PaymentRepositoryAdapter(
   val repository: PaymentJpaRepository
 ): PaymentRepository {
+  override fun save(domain: Payment): Payment {
+    val entity = PaymentMapper.toEntity(domain)
+    val saveEntity = repository.saveAndFlush(entity)
+
+    return PaymentMapper.toDomain(saveEntity)
+  }
+
+  override fun findByPaymentId(domain: Payment): Payment {
+    val findEntity = repository.findByIdOrNull(domain.id)
+      ?: throw Exception("결제건을 찾을 수 없습니다")
+
+    return PaymentMapper.toDomain(findEntity)
+  }
 }
