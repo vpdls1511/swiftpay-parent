87f844dbec63588ae19bfd27a959daca9778d51b
nGyu <vpdls1511@gmail.com>
2025-10-19 00:23:45 +0900

    refactor : Bean 주입 방식으로 변경

--- module/application/payment/src/main/kotlin/com/ngyu/swiftpay/payment/application/strategy/PaymentStrategyFactory.kt
+++ module/application/payment/src/main/kotlin/com/ngyu/swiftpay/payment/application/strategy/PaymentStrategyFactory.kt
@@ -2,19 +2,17 @@ package com.ngyu.swiftpay.payment.application.strategy
 
 import com.ngyu.swiftpay.core.domain.payment.model.PayMethod
 import com.ngyu.swiftpay.core.domain.payment.model.Payment
-import com.ngyu.swiftpay.payment.application.service.payment.PaymentBankService
-import com.ngyu.swiftpay.payment.application.service.payment.PaymentCardService
 import org.springframework.stereotype.Component
 
 @Component
 class PaymentStrategyFactory(
-  private val cardService: PaymentCardService,
-  private val bankService: PaymentBankService
+  private val cardStrategy: PaymentCardStrategy,
+  private val bankStrategy: PaymentBankStrategy
 ) {
   fun getStrategy(payment: Payment): PaymentStrategy {
     return when (payment.method) {
-      PayMethod.CARD -> PaymentCardStrategy(cardService)
-      PayMethod.BANK_TRANSFER -> PaymentBankStrategy(bankService)
+      PayMethod.CARD -> cardStrategy
+      PayMethod.BANK_TRANSFER -> bankStrategy
       else -> throw UnsupportedOperationException("지원하지 않는 결제수단 입니다.")
     }
   }
