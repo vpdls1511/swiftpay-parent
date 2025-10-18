8c626dfcb167d0a61cf55b0f21a1dbfa05955b7d
nGyu <vpdls1511@gmail.com>
2025-10-18 12:07:35 +0900

    feat : 결제 처리 시작 컨트롤러 추가

--- module/application/payment/src/main/kotlin/com/ngyu/swiftpay/payment/api/controller/PaymentController.kt
+++ module/application/payment/src/main/kotlin/com/ngyu/swiftpay/payment/api/controller/PaymentController.kt
@@ -2,9 +2,7 @@ package com.ngyu.swiftpay.payment.api.controller
 
 import com.ngyu.swiftpay.core.logger.logger
 import com.ngyu.swiftpay.payment.api.application.usecase.PaymentUseCase
-import com.ngyu.swiftpay.payment.api.dto.OrderCreateRequestDto
-import com.ngyu.swiftpay.payment.api.dto.OrderCreateResponseDto
-import com.ngyu.swiftpay.payment.api.dto.PaymentCredentials
+import com.ngyu.swiftpay.payment.api.dto.*
 import com.ngyu.swiftpay.payment.security.PaymentPrincipal
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
@@ -34,6 +32,18 @@ class PaymentController(
     val response = paymentUseCase.readyOrder(request)
     return ResponseEntity
       .status(HttpStatus.CREATED)
+      .body(response)
+  }
+
+  @PostMapping
+  fun confirmPayment(
+    @PaymentPrincipal principal: PaymentPrincipal,
+    @RequestBody request: PaymentRequestDto
+  ): ResponseEntity<PaymentResponseDto> {
+    log.info("결제 처리 시작")
+    val response = paymentUseCase.processing(request)
+    return ResponseEntity
+      .status(HttpStatus.ACCEPTED)
       .body(response)
   }
 }
