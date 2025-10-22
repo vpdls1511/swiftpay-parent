9623ad151da04f1834cb65e695666179cdcd85f3
nGyu <vpdls1511@gmail.com>
2025-10-22 20:28:06 +0900

    feat Escrow 도메인 생성

---
+++ module/application/payment/src/main/kotlin/com/ngyu/swiftpay/payment/application/service/EscrowService.kt
@@ -0,0 +1,33 @@
+package com.ngyu.swiftpay.payment.application.service
+
+import com.ngyu.swiftpay.core.domain.escrow.Escrow
+import com.ngyu.swiftpay.core.domain.escrow.EscrowRepository
+import com.ngyu.swiftpay.core.domain.payment.model.Payment
+import com.ngyu.swiftpay.core.logger.logger
+import jakarta.transaction.Transactional
+import org.springframework.stereotype.Service
+
+@Service
+class EscrowService(
+  private val escrowRepository: EscrowRepository
+) {
+
+  private val log = logger()
+
+  @Transactional
+  fun hold(payment: Payment): Escrow {
+    log.info("에스크로 예치 시작 | paymentId=${payment.paymentId}, amount=${payment.amount}")
+    val escrow = Escrow.hold(payment)
+    val savedEscrow = escrowRepository.save(escrow)
+    log.info("에스크로 예치 완료 | escrowId=${savedEscrow.escrowId}")
+    return savedEscrow
+  }
+
+  fun settle(escrow: Escrow): Escrow {
+    TODO("예치 로직 만들어야함")
+  }
+
+  fun refund(escrow: Escrow): Escrow {
+    TODO("환불 로직 만들어야함")
+  }
+}
