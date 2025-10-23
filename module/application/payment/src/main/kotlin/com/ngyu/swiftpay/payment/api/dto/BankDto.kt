0ae836fe1bc9ac1a995ddba4078b40ad33f03d8b
nGyu <vpdls1511@gmail.com>
2025-10-23 22:58:43 +0900

    feat : Dto 생성

---
+++ module/application/payment/src/main/kotlin/com/ngyu/swiftpay/payment/api/dto/BankDto.kt
@@ -0,0 +1,37 @@
+package com.ngyu.swiftpay.payment.api.dto
+
+import com.ngyu.swiftpay.core.domain.bank.BankAccount
+import com.ngyu.swiftpay.core.domain.bank.BankCode
+import io.swagger.v3.oas.annotations.Hidden
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.Size
+import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBlank
+
+@Schema(description = "계좌번호 생성 DTO")
+data class BankAccountCreateDto(
+
+  @field:NotBlank(message = "예금주명은 필수입니다")
+  @field:Size(min = 2, max = 100, message = "예금주명은 2~100자")
+  val holder: String,
+
+  @Hidden
+  val bankCode: BankCode = BankCode.SWIFT,
+)
+
+
+@Schema(description = "생성 완료된 계좌")
+data class BankAccountResponseDto(
+  val bankName: String,
+  val accountNumber: String,
+  val accountHolder: String
+) {
+  companion object {
+    fun fromDomain(domain: BankAccount, accountNumber: String): BankAccountResponseDto {
+      return BankAccountResponseDto(
+        bankName = domain.bankCode.label,
+        accountNumber = accountNumber,
+        accountHolder = domain.accountHolder,
+      )
+    }
+  }
+}
