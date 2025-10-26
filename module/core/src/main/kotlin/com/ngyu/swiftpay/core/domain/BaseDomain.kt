bdbad325c71b86fa2a11d7245c502e0ce2b2827a
nGyu <vpdls1511@gmail.com>
2025-10-26 10:08:07 +0900

    feat : 추상도메인 생성

---
+++ module/core/src/main/kotlin/com/ngyu/swiftpay/core/domain/BaseDomain.kt
@@ -0,0 +1,27 @@
+package com.ngyu.swiftpay.core.domain
+
+import java.io.Serializable
+
+/**
+ * 도메인 엔티티의 기본 추상 클래스
+ *
+ * ID 기반의 동등성(equality) 비교를 제공한다.
+ * 모든 도메인 엔티티는 이 클래스를 상속받아 ID를 기준으로 동일성을 판단한다.
+ *
+ * @param ID 엔티티의 식별자 타입 (Serializable이어야 함)
+ */
+abstract class BaseDomain<ID : Serializable> {
+
+  abstract fun id(): ID
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (other !is BaseDomain<*>) return false
+
+    return id() == other.id()
+  }
+
+  override fun hashCode(): Int {
+    return id().hashCode()
+  }
+}
