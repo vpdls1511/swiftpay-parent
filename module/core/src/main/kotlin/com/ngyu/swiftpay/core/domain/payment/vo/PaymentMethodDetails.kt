67b203895ff082daf8c5aa2701b37b0997d0c197
nGyu <vpdls1511@gmail.com>
2025-10-27 21:47:33 +0900

    refactor: PaymentMethodDetails nullable 필드 제거

--- module/core/src/main/kotlin/com/ngyu/swiftpay/core/domain/payment/vo/PaymentMethodDetails.kt
+++ module/core/src/main/kotlin/com/ngyu/swiftpay/core/domain/payment/vo/PaymentMethodDetails.kt
@@ -4,19 +4,17 @@ import com.ngyu.swiftpay.core.domain.payment.PaymentCardType
 
 // 결제 수단별 상세 정보
 sealed class PaymentMethodDetails {
-
   data class Card(
-    val cardNumber: String?,          // 카드번호 (토큰화...)
-    val cardExpiry: String?,          // 유효기간 (YYMM)
-    val cardCvc: String?,             // CVC
-    val installmentPlan: Int? = 0,    // 할부 개월 (0: 일시불)
-    val cardType: PaymentCardType?,
+    val cardNumber: String,          // 카드번호 (토큰화...)
+    val cardExpiry: String,          // 유효기간 (YYMM)
+    val cardCvc: String,             // CVC
+    val installmentPlan: Int = 0,    // 할부 개월 (0: 일시불)
+    val cardType: PaymentCardType,
     val useCardPoint: Boolean = false // 카드 포인트 사용 여부
   ) : PaymentMethodDetails()
 
   data class BankTransfer(
-    val bankCode: String?,        // 은행 코드
-    val accountNumber: String?    // 계좌번호
+    val bankCode: String,        // 은행 코드
+    val accountNumber: String    // 계좌번호
   ) : PaymentMethodDetails()
-
 }
