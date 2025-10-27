c08013752e0c63a216bd04400bddb15c6462e7e3
nGyu <vpdls1511@gmail.com>
2025-10-27 22:17:26 +0900

    feat : 도메인 생성 시 id 주입을 위한 sequence 추가

--- module/infrastructure/src/main/resources/sql/sequence.sql
+++ module/infrastructure/src/main/resources/sql/sequence.sql
@@ -5,3 +5,43 @@ CREATE SEQUENCE account_number_seq
     MINVALUE 1
     MAXVALUE 9999999999
     CYCLE;
+
+-- 가맹점 시퀀스 생성 (MySQL 8.0+)
+CREATE SEQUENCE merchant_number_seq
+    START WITH 1
+    INCREMENT BY 1
+    MINVALUE 1
+    MAXVALUE 9999999999
+    CYCLE;
+
+-- 결제 시퀀스 생성 (MySQL 8.0+)
+CREATE SEQUENCE payment_number_seq
+    START WITH 1
+    INCREMENT BY 1
+    MINVALUE 1
+    MAXVALUE 9999999999
+    CYCLE;
+
+-- 주문 시퀀스 생성 (MySQL 8.0+)
+CREATE SEQUENCE order_number_seq
+    START WITH 1
+    INCREMENT BY 1
+    MINVALUE 1
+    MAXVALUE 9999999999
+    CYCLE;
+
+-- 정산 시퀀스 생성 (MySQL 8.0+)
+CREATE SEQUENCE settlement_number_seq
+    START WITH 1
+    INCREMENT BY 1
+    MINVALUE 1
+    MAXVALUE 9999999999
+    CYCLE;
+
+-- 에스크로 시퀀스 생성 (MySQL 8.0+)
+CREATE SEQUENCE escrow_number_seq
+    START WITH 1
+    INCREMENT BY 1
+    MINVALUE 1
+    MAXVALUE 9999999999
+    CYCLE;
