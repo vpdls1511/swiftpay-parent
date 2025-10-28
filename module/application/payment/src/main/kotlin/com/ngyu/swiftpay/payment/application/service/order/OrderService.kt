05c99188a26108020237d2d2fefbcf0a9a6d0fd5
nGyu <vpdls1511@gmail.com>
2025-10-28 21:52:04 +0900

    refactor : 서비스 분리

---
+++ module/application/payment/src/main/kotlin/com/ngyu/swiftpay/payment/application/service/order/OrderService.kt
@@ -0,0 +1,31 @@
+package com.ngyu.swiftpay.payment.application.service.order
+
+import com.ngyu.swiftpay.core.common.logger.logger
+import com.ngyu.swiftpay.core.domain.order.Order
+import com.ngyu.swiftpay.core.port.OrderRepository
+import com.ngyu.swiftpay.core.port.SequenceGenerator
+import com.ngyu.swiftpay.payment.api.dto.OrderCreateRequestDto
+import com.ngyu.swiftpay.payment.api.dto.OrderCreateResponseDto
+import jakarta.transaction.Transactional
+import org.springframework.stereotype.Service
+
+@Service
+class OrderService(
+  private val orderRepository: OrderRepository,
+  private val sequenceGenerator: SequenceGenerator
+) {
+  private val log = logger()
+
+  @Transactional
+  fun createOrder(request: OrderCreateRequestDto): OrderCreateResponseDto {
+    log.info("주문서 생성 시작 | merchantId=${request.merchantId}, orderName=${request.orderName}, amount=${request.totalAmount}")
+    //TODO - 추후에 merchantId 검증 로직 필요
+    val orderSeq = sequenceGenerator.nextOrderId()
+    val orderId = Order.createOrderId(orderSeq)
+    val order = request.toDomain(orderSeq, orderId)
+    val savedDomain = orderRepository.save(order)
+
+    log.info("주문서 생성 완료 | orderId=${savedDomain.orderId}, merchantId=${savedDomain.merchantId}")
+    return OrderCreateResponseDto.fromDomain(savedDomain)
+  }
+}
