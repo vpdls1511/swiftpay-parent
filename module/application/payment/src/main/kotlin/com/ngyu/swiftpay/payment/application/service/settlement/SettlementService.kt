85cba676be4e341f3ede6913d2167a6ce3597bc0
nGyu <vpdls1511@gmail.com>
2025-12-30 14:17:06 +0900

    feat : 정산 대기상태 진입 가능

---
+++ module/application/payment/src/main/kotlin/com/ngyu/swiftpay/payment/application/service/settlement/SettlementService.kt
@@ -0,0 +1,35 @@
+package com.ngyu.swiftpay.payment.application.service.settlement
+
+import com.ngyu.swiftpay.core.common.logger.logger
+import com.ngyu.swiftpay.core.domain.escrow.Escrow
+import com.ngyu.swiftpay.core.domain.settlement.Settlement
+import com.ngyu.swiftpay.core.port.generator.SequenceGenerator
+import com.ngyu.swiftpay.core.port.repository.SettlementRepository
+import org.springframework.stereotype.Service
+
+@Service
+class SettlementService(
+  private val sequenceGenerator: SequenceGenerator,
+  private val settlementRepository: SettlementRepository
+) {
+  private val log = logger()
+
+  /**
+   * 정산을 위한 Settlement 저장
+   */
+  fun pending(escrow: Escrow, merchantId: String) {
+    val settlementSeq = sequenceGenerator.nextSettlementId()
+    val settlementId = Settlement.createSettlementId(settlementSeq)
+    escrow.settle(settlementSeq)
+
+    val settlement = Settlement.create(
+      seq = settlementSeq,
+      settlementId = settlementId,
+      merchantId = merchantId,
+      totalAmount = escrow.amount,
+      fee = escrow.amount / 10 // 수수료 10%
+    )
+
+    settlementRepository.save(settlement)
+  }
+}
