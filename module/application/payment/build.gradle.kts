118f81551e9d601c81dcf6968f8a255b73f145f5
nGyu <vpdls1511@gmail.com>
2025-12-30 18:02:54 +0900

    chore : 코루틴 사용을 위한 의존성 추가

--- module/application/payment/build.gradle.kts
+++ module/application/payment/build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("org.springframework.boot:spring-boot-starter-actuator")
   implementation("org.springframework.boot:spring-boot-starter-validation")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
 
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
