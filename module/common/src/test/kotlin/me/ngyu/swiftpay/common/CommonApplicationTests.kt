b97deae85333dfa53a8679d2ee1519b57f340bc2
nGyu <vpdls1511@gmail.com>
2026-02-07 12:08:07 +0900

    feat : 공통 로직 관리를 위한 common 모듈 추가

---
+++ module/common/src/test/kotlin/me/ngyu/swiftpay/common/CommonApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.ngyu.swiftpay.common
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommonApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,9 @@
 rootProject.name = "swiftpay-parent"
 
+include("module:application:auth")
 include("module:application:payment")
 
 include("module:core")
 include("module:infrastructure")
-include("module:auth")
 include("module:security")
+include("module:common")
