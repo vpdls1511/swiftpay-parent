41109502784c63da313235e3dfa8f6cef1a3b420
nGyu <vpdls1511@gmail.com>
2026-02-09 22:21:38 +0900

    feat : Account 기본값 적용

--- module/application/auth/src/main/kotlin/me/ngyu/swiftpay/auth/domain/account/Account.kt
+++ module/application/auth/src/main/kotlin/me/ngyu/swiftpay/auth/domain/account/Account.kt
@@ -2,16 +2,17 @@ package me.ngyu.swiftpay.auth.domain.account
 
 import jakarta.persistence.*
 import me.ngyu.swiftpay.common.domain.BaseEntity
+import java.util.*
 
 @Entity
 @Table(name = "account")
 class Account(
   @Column(nullable = false, unique = true, length = 36)
-  val uuid: String,
+  val uuid: String = UUID.randomUUID().toString(),
 
   @Enumerated(EnumType.STRING)
   @Column(nullable = false, length = 20)
-  var role: AccountRole,
+  var role: AccountRole = AccountRole.USER,
 
   @Enumerated(EnumType.STRING)
   @Column(nullable = false, length = 20)
