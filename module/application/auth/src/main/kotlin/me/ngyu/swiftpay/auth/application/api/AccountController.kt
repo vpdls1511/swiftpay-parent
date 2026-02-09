a8072318a3540c5db5bb425d00b8fd307ab4f603
nGyu <vpdls1511@gmail.com>
2026-02-09 22:36:44 +0900

    feat : 회원가입 컨트롤러 추가

---
+++ module/application/auth/src/main/kotlin/me/ngyu/swiftpay/auth/application/api/AccountController.kt
@@ -0,0 +1,24 @@
+package me.ngyu.swiftpay.auth.application.api
+
+import me.ngyu.swiftpay.auth.application.api.dto.AccountRequest
+import me.ngyu.swiftpay.auth.application.usecase.AccountUseCase
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.net.URI
+
+@RestController
+@RequestMapping("/v1/api/account")
+class AccountController(
+  private val accountUseCase: AccountUseCase,
+) {
+
+  @PostMapping("/register")
+  fun register(@RequestBody request: AccountRequest): ResponseEntity<Void> {
+    val uuid = accountUseCase.saveUser(request)
+    return ResponseEntity.created(URI.create("/accounts/${uuid}")).build()
+  }
+
+}
